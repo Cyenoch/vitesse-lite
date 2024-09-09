31f0eb180299f8ee97221d83138ab60b46e26323
Cyenoch <cyenoch@outlook.com>
2024-09-09 15:22:47 +0800

    remove useless code

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -11,14 +11,5 @@ defineOptions({
       <span class="i-line-md-hash mr-1" />
       Hello
     </Button>
-
-    <Dialog>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>123</DialogTitle>
-          <DialogDescription>321</DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
   </div>
 </template>
