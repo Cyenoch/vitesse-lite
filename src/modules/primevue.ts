b08a72f25fcc90d197dadf53aed527a5c3c9d466
Cyenoch <cyenoch@outlook.com>
2024-12-21 14:00:51 +0800

    feat: enhance PrimeVue theme configuration with semantic color scheme support

--- src/modules/primevue.ts
+++ src/modules/primevue.ts
@@ -1,11 +1,21 @@
 import type { App } from 'vue'
+import { definePreset } from '@primevue/themes'
 import Aura from '@primevue/themes/aura'
 import PrimeVue from 'primevue/config'
 
 export function install(app: App) {
   app.use(PrimeVue, {
     theme: {
-      preset: Aura,
+      preset: definePreset(Aura, {
+        components: {},
+        semantic: {
+          colorScheme: {
+            light: {
+
+            },
+          },
+        },
+      }),
       options: {
         darkModeSelector: '.dark',
       },
