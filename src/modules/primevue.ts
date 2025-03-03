39b2a9d5bb9651faff8844bc48a7954352d8ad5f
Cyenoch <jgbingzi@outlook.com>
2025-03-03 11:58:55 +0900

    fix: import

--- src/modules/primevue.ts
+++ src/modules/primevue.ts
@@ -1,6 +1,6 @@
 import type { App } from 'vue'
-import { definePreset } from '@primevue/themes'
-import Aura from '@primevue/themes/aura'
+import { definePreset } from '@primeuix/themes'
+import Aura from '@primeuix/themes/aura'
 import PrimeVue from 'primevue/config'
 
 export function install(app: App) {
