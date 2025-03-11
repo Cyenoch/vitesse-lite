85f74bb405199520a68e494bdbd8bad79c583d33
Cyenoch <jgbingzi@outlook.com>
2025-03-11 11:48:11 +0800

    feat: add hot module replacement support for Vue Router

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -1,11 +1,16 @@
 import type { App } from 'vue'
+import type { RouteRecordRaw } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { createRouter, createWebHistory } from 'vue-router/auto'
-import { routes } from 'vue-router/auto-routes'
+import { handleHotUpdate, routes } from 'vue-router/auto-routes'
+
+function generateLayoutsRoutes(originRoutes: RouteRecordRaw[]) {
+  return setupLayouts(originRoutes)
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: setupLayouts(routes),
+  routes: generateLayoutsRoutes(routes),
   scrollBehavior(to, from, savedPosition) {
     if (to.meta.keepPreviousScrollPosition) {
       return
@@ -18,6 +23,15 @@ const router = createRouter({
     }
   },
 })
+
+if (import.meta.hot) {
+  handleHotUpdate(router, (newRoutes) => {
+    const withLayoutsRoutes = generateLayoutsRoutes(newRoutes)
+    withLayoutsRoutes.forEach((route) => {
+      router.addRoute(route)
+    })
+  })
+}
 
 export function install(app: App) {
   app.use(router)
